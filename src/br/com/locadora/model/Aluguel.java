88cc5306b3a6dd3392cd679121ceda12650f06d9
Pedro <pedrolucassc23@gmail.com>
2025-11-30 16:53:35 -0300

    ideia inicial da classe aluguel

--- src/br/com/locadora/model/Aluguel.java
+++ src/br/com/locadora/model/Aluguel.java
@@ -1,4 +1,28 @@
 package br.com.locadora.model;
 
 public class Aluguel {
+    // Identificador único do aluguel
+    private int id; 
+    
+    // Relacionamento Cliente
+    private Cliente cliente; 
+    
+    // Relacionamento com Jogo. Pode ser uma lista se o aluguel 
+    //private Jogo jogo; 
+    
+    
+    private LocalDate dataInicio;
+    private LocalDate dataDevolucaoPrevista;
+    private LocalDate dataDevolucaoReal; // Pode ser null se ainda não devolvido
+    
+    
+    private double valorTotal;
+    private boolean isAtivo;
+
+    
+    // Método de Negócio Exemplo:
+    public boolean isAluguelAtivo() {
+        // Um aluguel está ativo se a data de devolução real for nula 
+        // e/ou a data prevista ainda não foi atingida.
+        return dataDevolucaoReal == null;
 }
