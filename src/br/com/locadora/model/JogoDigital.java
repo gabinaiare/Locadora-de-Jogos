e7dbcc893001fab6a756ed8b6ce6fe09deecdefe
pedrokaut <lexus04111@gmail.com>
2025-12-01 15:31:56 -0300

    feat: Adicionando Atributos e inicializando o construtor

--- src/br/com/locadora/model/JogoDigital.java
+++ src/br/com/locadora/model/JogoDigital.java
@@ -1,4 +1,13 @@
 package br.com.locadora.model;
 
 public class JogoDigital extends Jogo {
+    private String plataforma;
+    private double tamanhoArquivo; 
+
+    public JogoDigital(String nome,int anoLancamento,int classificacao,String descricao,String tipo,float valorBase, String plataforma, double tamanhoArquivo) {
+        super(nome,anoLancamento,classificacao,descricao,tipo,valorBase);
+        this.plataforma = plataforma;
+        this.tamanhoArquivo = tamanhoArquivo;
+    }
+
 }
