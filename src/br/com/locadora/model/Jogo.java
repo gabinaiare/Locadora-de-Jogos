7ee21553017f7089ca3cc0e0fdd5863859b4762c
AliceSantoi <alicelira2016@gmail.com>
2025-12-01 14:54:00 -0300

    feat: adicionando atributos na classe Jogo

--- src/br/com/locadora/model/Jogo.java
+++ src/br/com/locadora/model/Jogo.java
@@ -1,4 +1,12 @@
 package br.com.locadora.model;
 
 public abstract class Jogo {
+    private String nome;
+    private int anoLancamento;
+    private int classificacao;
+    private String descricao;
+    private String tipo;
+    private float valorBase;
+
+    public abstract void CalcularValorAluguel(Aluguel aluguel);
 }
