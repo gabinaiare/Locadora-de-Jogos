56cee6f89eadb0c5dde8283922afcb51ec3026bf
Pablo Ryan <pabloryanlopes@gmail.com>
2025-12-02 11:00:06 -0300

    feat
    adicionei o construtor de Aluguel com as validações iniciais

--- src/br/com/locadora/model/Aluguel.java
+++ src/br/com/locadora/model/Aluguel.java
@@ -9,4 +9,19 @@ public class Aluguel {
     private LocalDate dataAluguel;
     private LocalDate dataDevolucao;
     private Double valorTotal;
+    
+    public Aluguel (Cliente cliente, List<Jogo> jogos, LocalDate dataAluguel, LocalDate dataDevolucao){
+        if (cliente == null) {
+                throw new IllegalArgumentException("Cliente não pode ser nulo!");
+            }
+            if (jogos == null || jogos.isEmpty()) {
+                throw new IllegalArgumentException("Deve haver pelo menos um jogo no aluguel.");
+            }
+            if (dataAluguel == null || dataDevolucao == null) {
+                throw new IllegalArgumentException("Datas de aluguel e devolução não podem ser nulas!");
+            }
+            if (dataDevolucao.isBefore(dataAluguel)) {
+                throw new IllegalArgumentException("Data de devolução não pode ser antes da data de aluguel.");
+            }
+        }
 }
