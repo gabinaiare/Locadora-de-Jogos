cfbc99edd0aaf45d4301a302d22ad6e825fcd77f
pedrokaut <lexus04111@gmail.com>
2025-12-08 19:21:40 -0300

    feat: adicionando construtor & dependencias

--- src/br/com/locadora/view/MenuJogo.java
+++ src/br/com/locadora/view/MenuJogo.java
@@ -1,7 +1,26 @@
 package br.com.locadora.view;
 
+import br.com.locadora.model.*;
+import br.com.locadora.service.JogoService;
+import br.com.locadora.service.ClienteService;
+import br.com.locadora.service.LocadoraService;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class MenuJogo {
+     private Scanner input;
+     private JogoService jogoService;
+     private ClienteService clienteService;
+     private LocadoraService locadoraService;
+
+    public MenuJogo(JogoService jogoService, ClienteService clienteService, LocadoraService locadoraService) {
+        this.input = new Scanner(System.in);
+        this.jogoService = jogoService;
+        this.clienteService = clienteService;
+        this.locadoraService = locadoraService;
+    }
 
     
 }
