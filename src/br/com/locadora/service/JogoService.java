b8c4e48f0a43aaeddb00da5d54db769fef26590f
pedrokaut <lexus04111@gmail.com>
2025-12-08 18:52:06 -0300

    feat: implementa lógica de criação e validação de jogos

--- src/br/com/locadora/service/JogoService.java
+++ src/br/com/locadora/service/JogoService.java
@@ -19,5 +19,32 @@ public class JogoService {
         jogoRepository.salvar(jogo);
     }  
 
+      public void criarJogo(String nome, int anoLancamento, int classificacao, String descricao,String tipo, float valorBase, Plataforma plataforma, Double tamanhoArquivo, String estadoFisico) {
+
+        Jogo jogo;
+
+        if (tipo.equalsIgnoreCase("Digital")) {
+            if (tamanhoArquivo == null) {
+                throw new IllegalArgumentException("Tamanho do arquivo obrigatório para digitais!");
+            }
+
+            jogo = new JogoDigital(nome, anoLancamento, classificacao, descricao, tipo,
+                    valorBase, tamanhoArquivo, 0, plataforma);
+
+        } else if (tipo.equalsIgnoreCase("Fisico")) {
+            if (estadoFisico == null || estadoFisico.isBlank()) {
+                throw new IllegalArgumentException("Estado obrigatório para jogo físico!");
+            }
+
+            jogo = new JogoFisico(nome, anoLancamento, classificacao, descricao, tipo,
+                    valorBase, estadoFisico, 0, plataforma);
+
+        } else {
+            throw new IllegalArgumentException("Esse tipo não existe! Use: Digital ou Fisico");
+        }
+
+        salvar(jogo);
+    }
+
    
 }
