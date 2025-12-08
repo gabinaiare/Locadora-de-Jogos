38ab8fe82c8f309dc2be4fcbd84d4614b7738b82
pedrokaut <lexus04111@gmail.com>
2025-12-08 18:10:09 -0300

    feat: iniciando jogo service e mudanças no jason

---
+++ src/br/com/locadora/service/JogoService.java
@@ -0,0 +1,12 @@
+package br.com.locadora.service;
+
+import br.com.locadora.interfaces.IJogo;
+import br.com.locadora.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JogoService {
+
+   
+}
